e566702e763805f5d0e5146585f34c9599dfb6e4
Travis Johnson <travis@thisguy.codes>
2016-03-11 11:42:28 -0800

    conditional sudo

--- run.sh
+++ run.sh
@@ -6,5 +6,13 @@ cd koupler-0.2.5-SNAPSHOT
 tar -cv * > ../koupler-0.2.5-SNAPSHOT.tar
 cd ../../..
 
-docker build -t koupler .
-docker run --env-file=aws.env --rm -p=4567:${2} -it koupler -streamName "${1}" -http
+# Only use sudo if there's no configured remote host
+if [ -n "${DOCKER_HOST}" ]
+then
+	PRE=''
+else
+	PRE='sudo'
+fi
+
+${PRE} docker build -t koupler .
+${PRE} docker run --env-file=aws.env --rm -p=4567:${2} -it koupler -streamName "${1}" -http
